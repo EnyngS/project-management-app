37112d4e79cc5405855ab7d6953af88390601bd4
Enyng <susikovvlad@gmail.com>
2022-05-21 20:00:15 +0400

    feat: btn new board & delete

--- my-app/src/components/LoginPageContainer/LoginPageContainer.tsx
+++ my-app/src/components/LoginPageContainer/LoginPageContainer.tsx
@@ -7,87 +7,86 @@ import Error from '../Error/Error';
 import LoginPage from './LoginPage/LoginPage';
 const axios = require('axios').default;
 
-const LoginPageContainer:FC = () => {
-	const state = useSelector((state: any) => state.auth)
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
+const LoginPageContainer: FC = () => {
+  const state = useSelector((state: any) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	interface IUser {
-		isAuth: boolean;
-		id: string;
-		username: string;
-		login: string,
-		email: string,
-		password: string,
-		token: string,
-	 }
-	let user: IUser;
-	let setUser: any;
+  interface IUser {
+    isAuth: boolean;
+    id: string;
+    username: string;
+    login: string;
+    email: string;
+    password: string;
+    token: string;
+  }
+  let user: IUser;
+  let setUser: any;
 
-	[user, setUser] = useState({
-		isAuth: false,
-		id: '',
-		username: '',
-		login: '',
-		email: '',
-		password: '',
-		token: ''
-	});
+  [user, setUser] = useState({
+    isAuth: false,
+    id: '',
+    username: '',
+    login: '',
+    email: '',
+    password: '',
+    token: '',
+  });
 
-	useEffect( () => {
-		if(user.isAuth){
-			axios.post('https://quiet-bastion-49623.herokuapp.com/signin',{
-			"login": user.login,
-			"password": user.password,
-			})
-			.then((res:any)=>{
-				setUser({
-					...user,
-					token: res.data.token
-				})
-				axios.get(`https://quiet-bastion-49623.herokuapp.com/users`,{
-				headers:{
-					"Authorization": `Bearer ${res.data.token}`
-				}
-			})
-			.then((res:any) => {
-				let [userId] = res.data.filter((el: any) => (el.login === user.login))
-				let resultUser = {
-					...user,
-					id: userId.id,
-					username: userId.name,
-					login: userId.login,
-				}
+  useEffect(() => {
+    if (user.isAuth) {
+      axios
+        .post('https://quiet-bastion-49623.herokuapp.com/signin', {
+          login: user.login,
+          password: user.password,
+        })
+        .then((res: any) => {
+          localStorage.setItem('token', res.data.token);
+          setUser({
+            ...user,
+            token: res.data.token,
+          });
+          axios
+            .get(`https://quiet-bastion-49623.herokuapp.com/users`, {
+              headers: {
+                Authorization: `Bearer ${res.data.token}`,
+              },
+            })
+            .then((res: any) => {
+              let [userId] = res.data.filter((el: any) => el.login === user.login);
+              let resultUser = {
+                ...user,
+                id: userId.id,
+                username: userId.name,
+                login: userId.login,
+              };
 
-				return(
-					dispatch(signin(resultUser)),
-					setUser({
-						isAuth: false,
-						id: '',
-						username: '',
-						login: '',
-						email: '',
-						password: '',
-						token: ''
-					}),
-					navigate('/main')
-				)
-			})
-			.catch((er: any)=> {
-				console.log(er)
-			})
-			})
-			.catch((error: any) => {
-				return(
-					navigate('/error'),
-					<Error {...error.response.data.message} />
-				)
-			})
-		}
-		
-	}, [user] )
+              return (
+                dispatch(signin(resultUser)),
+                setUser({
+                  isAuth: false,
+                  id: '',
+                  username: '',
+                  login: '',
+                  email: '',
+                  password: '',
+                  token: '',
+                }),
+                navigate('/main')
+              );
+            })
+            .catch((er: any) => {
+              console.log(er);
+            });
+        })
+        .catch((error: any) => {
+          return navigate('/error'), (<Error {...error.response.data.message} />);
+        });
+    }
+  }, [user]);
 
-  return (<LoginPage addUser = {setUser}/>);
+  return <LoginPage addUser={setUser} />;
 };
 
 export default LoginPageContainer;
--- my-app/src/store/sliceBoards.tsx
+++ my-app/src/store/sliceBoards.tsx
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 
-export type BoardPrevType = { id: string; title: string; description: string };
+export type BoardPrevType = { id?: string; title: string; description: string };
 
 export const BoardPrev: BoardPrevType[] = [
   {
@@ -22,24 +22,39 @@ export const BoardPrev: BoardPrevType[] = [
 const initialState = {
   boards: BoardPrev,
 };
-// -----когда будет авторизация
+
 export const PostBoards = createAsyncThunk('boadrs/PostBoards', async function (e: BoardPrevType) {
   const response = await fetch(`https://quiet-bastion-49623.herokuapp.com/boards`, {
-    method: 'POST', // или 'PUT'
-    body: JSON.stringify(e), // данные могут быть 'строкой' или {объектом}!
+    method: 'POST',
+    body: JSON.stringify(e),
     headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
       'Content-Type': 'application/json',
     },
   });
 });
-// ----------------------------
+export const deleteBoard = createAsyncThunk(
+  'boadrs/deleteBoard',
+  async function (id: string | undefined) {
+    const response = fetch(`https://quiet-bastion-49623.herokuapp.com/boards/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+  }
+);
+
 export const GetAllBoards = createAsyncThunk('boadrs/GetAllBoards', async function () {
   const response = await fetch(`https://quiet-bastion-49623.herokuapp.com/boards`, {
-    method: 'GET', // или 'PUT'
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
   });
   const data: BoardPrevType[] = await response.json();
-  //   return data;
-  return BoardPrev;
+  return data;
 });
 
 const sliceBoards = createSlice({
@@ -53,5 +68,4 @@ const sliceBoards = createSlice({
   },
 });
 
-// export const { getAll } = sliceBoards.actions;
 export default sliceBoards.reducer;
